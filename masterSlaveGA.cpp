2b765268998877d8c4f1b775589d6eed7940e107
Emma Smith <emmarsmi@usc.edu>
2017-04-22 21:09:59 -0700

    Working! ish, no seg faults but capacity is never reached

--- masterSlaveGA.cpp
+++ masterSlaveGA.cpp
@@ -17,8 +17,8 @@ using namespace std;
 // g++ -lpthread -o masterslave masterSlaveGA.cpp
 static bool check_fav(string fav, string a);
 static void* count_fav( void* threadarg );
-static void mutate(string* a,int index);
-static void swap(struct elem* a, struct elem* b, int start);
+static void mutate(int i, int j, int index);
+static void swap(struct elem a, struct elem b, int start);
 
 vector< vector<string> > m_vspace;
 
@@ -56,20 +56,21 @@ int main(int argc, char** argv)
 	cout << "n " << n << ", chunk_size " << chunk_size << ", fav = " << fav << ", numBlocks " << numBlocks << "\n" ;
     
     //Initial state of the matrix: even rows are bits of size n all 0’s, size of n bits all 1’s.
+	//TODO: make the strings we push back relate to the input of n!!!!!
     for (int i =0; i < m_size; i++) {
         vector<string> myvector;
         for (int j = 0; j < m_size; j++) {
             if(i%2 == 0){
-				myvector.push_back("1111");
+				myvector.push_back("11111111");
             }
             else {
-				myvector.push_back("0000");
+				myvector.push_back("00000000");
             }
         }
         m_vspace.push_back(myvector);
     }
     	
-    cout << "Done initializing m_vspace";
+    cout << "Done initializing m_vspace" << endl;
 	
     //pthreads
     struct thread_data thread_data_array[num_threads];
@@ -85,36 +86,35 @@ int main(int argc, char** argv)
     while(!fav_capacity) 
     {
 		cout << "beginning of while not capacity loop" << endl;
-        vector<struct elem*> matingPool;
+        vector<struct elem> matingPool;
 
         //copy over m_vspace into matingPool vector;
         for(int i=0; i<m_size; i++) {
             for(int j=0; j<m_size;  j++){
-                struct elem* e;
-                e->x = i;
-                e->y = j;
-                e->dna = m_vspace[i][j];
+                struct elem e;
+                e.x = i;
+                e.y = j;
+                e.dna = m_vspace[i][j];
                 matingPool.push_back(e);
             }
         }
         printf("after pushback \n");
-        for(int k =0; k<m_size*m_size/2 ; k++) {
-            printf("in for mating, k= %d \n", k);
-            cout << "Mating Pool size " << matingPool.size() << "\n";
+        while(matingPool.size() > 0) {
+            //cout << "In mating while, Mating Pool size " << matingPool.size() << "\n";
             //crossover - for all in mating pool, pick a random partner, switch one random block
             int firstIndex = rand()%(matingPool.size());
             int secondIndex = rand()%(matingPool.size());
             while(firstIndex==secondIndex) {
                 secondIndex = rand()%(matingPool.size());
             }
-            printf("first index = %d , second index = %d \n", firstIndex, secondIndex);
+            //printf("first index = %d , second index = %d \n", firstIndex, secondIndex);
                  
             //Recombination - save the new values determined by crossover
             int blockID = rand()%numBlocks;
             swap(matingPool[firstIndex], matingPool[secondIndex], (blockID*chunk_size));
-            printf("outside of swap \n");
+            //printf("outside of swap \n");
             
-            cout << "Mating Pool size before erases " << matingPool.size() << "\n";
+            //cout << "Mating Pool size before erases " << matingPool.size() << "\n";
             if(firstIndex > secondIndex) {
                 matingPool.erase(matingPool.begin() + firstIndex);
                 matingPool.erase(matingPool.begin() + secondIndex);
@@ -123,16 +123,20 @@ int main(int argc, char** argv)
                 matingPool.erase(matingPool.begin() + secondIndex);
                 matingPool.erase(matingPool.begin() + firstIndex);
             }
-            cout << "Mating Pool size after erases " << matingPool.size() << "\n";
-        }//end of mating for loop - all paired up
+            //cout << "Mating Pool size after erases " << matingPool.size() << "\n";
+        }//end of mating while loop - all paired up
         
+		cout << "before mutate" << endl;
         //Mutation - 10% chance to flip one random bit
-        for (int i = 0; i < m_size; i++) {
-            for(int j=0; j<m_size; j++){  
+        for (int i=0; i < m_size; i++) {
+            for(int j=0; j< m_size; j++){  
                 int chance = rand()%100;
                 if(chance < 10) {
                     int bit = rand()%n;
-                    mutate(&m_vspace[i][j], bit);
+					//cout << "calling mutate on m_vspace i = " << i << " j = " << j <<  "index = " << bit << endl; 
+					//cout << "m_vspace at that index = " << m_vspace[i][j] << endl;
+                    mutate(i, j, bit);
+					//cout << "new m_vspace at that index = " << m_vspace[i][j] << endl;
                 }
             }
         }
@@ -171,6 +175,8 @@ int main(int argc, char** argv)
         if(cap >= .7) {
             fav_capacity = true;
         }
+		cout << "END OF THIS GENERATION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+		cout << "capacity = " << cap << endl << endl;
     } //end of capacity while - all done with generations
 	
     //print results
@@ -185,27 +191,29 @@ int main(int argc, char** argv)
                                                                   
 // functions/////////
 //recombination
-void swap(struct elem* a, struct elem* b, int start)
+void swap(struct elem a, struct elem b, int start)
 {
-    string temp = a->dna;
+    string temp = a.dna;
     for(int i = start; i < start+chunk_size;i++) {
-        a->dna[i] = b->dna[i];
-        b->dna[i] = temp[i];
+        a.dna[i] = b.dna[i];
+        b.dna[i] = temp[i];
     }
-    //copys elem->dna into the m_vspace[x][y]
-    m_vspace[a->x][a->y] = a->dna;
-    m_vspace[b->x][b->y] = b->dna;
+    //copys elem dna into the m_vspace[x][y]
+    m_vspace[a.x][a.y] = a.dna;
+    m_vspace[b.x][b.y] = b.dna;
     
 }
                                                                            
-void mutate(string* a, int index)
+void mutate(int i, int j, int index)
 {
-    string localString(a[index]);
-    if (localString == "1")
-        a[index] = "0";
-    else
-        a[index] = "1";
-    
+    string localString = m_vspace[i][j];
+    if (localString.at(index) == '1') {
+        localString.at(index) = '0';
+	}
+    else {
+        localString.at(index) = '1';
+	}
+	m_vspace[i][j] = localString;
 }
                                                                                
 //THREAD FUNCTION
